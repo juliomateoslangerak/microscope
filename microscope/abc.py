5c6bcfa29ea07d9ef4951342229d4d2955d454f5
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-08-18 15:17:36 +0200

    Added a debug log entru when changing filterwheel position

--- microscope/abc.py
+++ microscope/abc.py
@@ -1290,6 +1290,7 @@ class FilterWheel(Device, metaclass=abc.ABCMeta):
     @position.setter
     def position(self, new_position: int) -> None:
         if 0 <= new_position < self.n_positions:
+            _logger.debug("Setting position to %d", new_position)
             return self._do_set_position(new_position)
         else:
             raise ValueError(
